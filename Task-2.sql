fe94d0b4ff9baa46683efda2783f5c4be78a4d8b
MATI UL REHMAN <74356765+Matiz009@users.noreply.github.com>
2021-09-21 22:31:43 +0500

    task-2 added with solution;

--- Task-2.sql
+++
@@ -1,6 +0,0 @@
-Use Office;
-CREATE TABLE Employee(name char(30),department char(10),salary varchar(50));
-INSERT into Employee(name,department,salary) VALUES ('Ahmad Azam','CS','45000');
-INSERT into Employee(name,department,salary) VALUES ('John Eston','Finance','35000');
-SELECT * FROM Employee;
-SELECT name FROM Employee;
